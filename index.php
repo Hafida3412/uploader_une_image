c7ed2d9504d2776da38024808e844399d0401fbb
Hafida3412 <lounsy81@gmail.com>
2024-06-14 14:40:18 +0200

    uploader/gérer les erreurs et vérif de l'extension

--- index.php
+++ index.php
@@ -17,7 +17,56 @@ $size = $_FILES['file']['size'];
 $error = $_FILES['file']['error'];//laisser tel quel, ça affiche une erreur/PHP ne recoonaît pas l'index (ici "file") car il n'y a rien dans la variable $_FILES. L’index « file » n’existe donc pas. 
 //pour corriger cette erreur, il faut vérifier si "file" existe
 //Maintenant que l’on a stocké les valeurs qui nous intéresse on va pouvoir les manipuler.
+
+//UPLOADER L IMAGE DANS UN DOSSIER
+
+/*Il faut créer un dossier "upload" à la racine du projet
+Il faut utiliser la fonction PHP "move_uploades_file". Cette fonction attend 2 paramètres:
+le chemin du fichier que l’on veut uploader et le chemin vers lequel on souhaite l’uploader.
+ex: move_uploaded_file(<< C:\Dev\wamp64\tmp\php84BB.tmp >>, <<./upload/image.jpg >>)
+*/
+
+/* - > on rajoute à la suite des variables: 
+move_uploades_file($tmpName, './upload/'.$name);//L'IMAGE EST UPLOADEE*/
+
+//GERER LES ERREURS
+
+/*Pour le moment on peut uploader n'importe quel type de fichier*/
+
+/*Vérification sur l'extension du fichier
+on rajoute à la suite des variables:
+*/
+$tabExtension = explode('.', $name);
+$extension = strtolower(end($tabExtension));/*la fonction strtolower met en minuscule une châine de caractère.
+ça permet de ne pas avoir de problème de comparaison. Ce n'est pas un souci si qq met un fichier
+avec une extension JPG, Jpg, jpG....
+
+/*la fonction explode permets de découper une chaîne de caractère en plusieurs morceaux à partir d’un délimiteur.
+ex: explode(« . » , « 10.25.65.20 ») permet de découper la chaîne de caractère à chaque fois qu’il y a un « . ». 
+On obtient alors le tableau [« 10 », « 25 », « 65 », « 20 »].
+
+On fait pareil pour le nom de notre fichier: explode(« . » , « image.jpg »)
+ça donne un tableau avec 2 éléments, comme ceci [« image », « jpg »]. 
+Il faut donc récupérer le dernier élément de ce tableau avec la fonction end().*/
+
+//VERIFICATION DE L EXTENSION
+
+/*On rajoute à la suite:*/
+
+//TABLEAU DES EXTENSIONS QUE L ON ACCEPTE
+$extensions = ['jpg', 'png', 'jpeg', 'gif'];
+
+/*On s'assure que l’extension du fichier envoyé se trouve bien dans ce tableau grâce 
+à la fonction in_array. Sinon on affiche un message d'erreur*/
+if(in_array($extension, $extensions)){
+    move_uploades_file($tmpName, './upload/'.$name)
 }
+else{
+    echo "Mauvaise exteniosn";
+}
+}
+
+
 ?>
 <!DOCTYPE html>
 <html>
